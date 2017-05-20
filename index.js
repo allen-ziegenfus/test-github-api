d7c6ef6a5a1312b6dc506adc5505cea9b5278918
allen-ziegenfus <allen.ziegenfus@liferay.com>
2017-05-20 09:02:56 +0200

    got it working

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+// TEMPLATE URIS
+// error handling
+
 var request = require('request');
 var fs = require("fs");
 
-function runTest(url, name) {
+var configFile = "./config.json";
+try {
+    config = JSON.parse(fs.readFileSync(configFile));
+} catch (error) {
+    logger.error("Please create config file " + configFile + " with the following syntax in the current directory");
+    logger.error(JSON.stringify(configDummy) + "\n");
+    throw new Error("Could not find config file: " + configFile);
+}
+
+function git_api_get(config, endpoint, name) {
     request.get({
-        url: url,
+        url: config.github_server + endpoint,
+        headers: {
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": "token " + config.github_token,
+            "User-Agent": config.github_user
+        },
         time: true
     }, function(err, response) {
         var json = JSON.parse(response.body);
@@ -12,19 +29,15 @@ function runTest(url, name) {
     });
 }
 
-var config = {
-    server: "https://api.punkapi.com/v2"
-};
-
 
 var testUrls = {
-    random: "/beers/random"
+    azuser: "/users/allen-ziegenfus"
 };
 
 function runTests(config, testUrls) {
     for (prop in testUrls) {
         console.log("Testing " + prop);
-        runTest(config.server + testUrls[prop], prop);
+        git_api_get(config, testUrls[prop], prop);
     }
 }
 
